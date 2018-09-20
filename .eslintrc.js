ef5422241274936b01749f8b9bc39fc576ed3b5c
Breci <brice.culas@edu.gobelins.fr>
2018-09-21 00:53:50 +0200

    added link to twitch policies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
+/*
+Following rules at https://dev.twitch.tv/docs/extensions/guidelines-and-policies/
+ */
+
 var OFF = 0, WARN = 1, ERROR = 2;
 const ENV = process.env.NODE_ENV;
+
 module.exports = exports = {
     env: {
         'browser': true,    // for the browser
