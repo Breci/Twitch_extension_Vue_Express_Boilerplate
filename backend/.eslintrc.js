fa764d9e32ddbf4366acd37a000fdf837bdbb39f
Breci <brice.culas@edu.gobelins.fr>
2018-10-14 13:38:50 +0200

    improved eslint config

---
+++ backend/.eslintrc.js
@@ -0,0 +1,15 @@
+/*
+Following rules at https://dev.twitch.tv/docs/extensions/guidelines-and-policies/
+ */
+
+var OFF = 0, WARN = 1, ERROR = 2;
+const ENV = process.env.NODE_ENV;
+
+module.exports = exports = {
+    env: {
+        "node": true,
+        "es6": true
+    },
+    "rules": {
+    }
+};
